fe99386fc06754f07704dc3e7bbf21ba10581956
cabrooks18 <114538088+cabrooks18@users.noreply.github.com>
2022-12-22 18:31:30 -0500

    1 file modified

--- src/Carousel.jsx
+++ src/Carousel.jsx
@@ -8,36 +8,36 @@ function App() {
     //   caption: "Salad"
     // },
     {
-      image: "https://images.unsplash.com/photo-1565299624946-b28f40a0ae38?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=781&q=80",
-      caption: "Pizza"
+      image: "https://photos.google.com/photo/AF1QipN_HY0Cg_Fff2VFu1A-RPJ1z2wTP5MCsBUrRHz-",
+      caption: "Son"
     },
     {
-      image: "https://images.unsplash.com/photo-1565958011703-44f9829ba187?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80",
-      caption: "Dessert"
+      image: "https://photos.google.com/photo/AF1QipP7PRd4xH99nBgPabIpXAuL3PUF1gLbVWTUK9K9",
+      caption: "Son & Fam "
     },
     {
-      image: "https://images.unsplash.com/photo-1499028344343-cd173ffc68a9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80",
-      caption: "Hambuger"
+      image: "hhttps://photos.google.com/photo/AF1QipNqEemhV2bC1u8Mngz108UW0M5EzFEMLhmnPAtL",
+      caption: "Grands"
     },
     {
-      image: "httpshttps://images.unsplash.com/photo-1481070555726-e2fe8357725c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80://i.natgeofe.com/n/f7732389-a045-402c-bf39-cb4eda39e786/scotland_travel_4x3.jpg",
-      caption: "Fish"
+      image: "https://photos.google.com/photo/AF1QipOL6SC9eL9e_19E4oSV35Z9CvWGDbIUIBC279hX",
+      caption: "Son & Fam2"
     },
     {
-      image: "https://images.unsplash.com/photo-1432139509613-5c4255815697?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=685&q=80",
-      caption: "Steak"
+      image: "https://photos.google.com/photo/AF1QipOSMg0WYXjlZrBvwOekalSP9ToST1JgCvaegdOY",
+      caption: "Boss Lady"
     },
     {
-      image: "https://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
-      caption: "Chocolate "
+      image: "httphttps://photos.google.com/photo/AF1QipPgZQCzaKoFIl1fYlIsix5WCeLaN-_Rw2weh9qXs://images.unsplash.com/photo-1563805042-7684c019e1cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzJ8fGZvb2R8ZW58MHx8MHx8&auto=format&fit=crop&w=800&q=60",
+      caption: "Us"
     },
     {
-      image: "https://images.unsplash.com/photo-1604152135912-04a022e23696?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-      caption: "Soup"
+      image: "https://photos.google.com/photo/AF1QipMZ633d8AWpTFMiScPVuIOJsIfUa2nZ_c32wB-l",
+      caption: "Son2"
     },
     {
-      image: "hhttps://images.unsplash.com/photo-1560684352-8497838a2229?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=828&q=80",
-      caption: "Seafood Soup"
+      image: "https://photos.google.com/photo/AF1QipOpYk56f1v-wyw51ovVFAWXT_FTN8FwRpef-n1L",
+      caption: "Grandbaby"
     }
   ];
 
